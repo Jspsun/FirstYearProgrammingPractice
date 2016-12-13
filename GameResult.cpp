bdc9eb4be35a6c9e82a44b99e868561a5f646272
Jspsun <Jspsun@gmail.com>
2016-12-13 00:11:05 -0500

    minor changes

--- GameResult.cpp
+++ GameResult.cpp
@@ -1,25 +1,27 @@
-struct gameResultType{
-    unsigned int numToss;
-    unsigned int amount;
+struct gameResultType {
+  unsigned int numToss;
+  unsigned int amount;
 };
 
-gameResultType coinToss(unsigned int initial, unsigned int target){
-    //initialize variables
-    gameResultType game;
-    game.numToss = 0;
-    game.amount = initial;
-    int betAmount = 0;
-    bool coin;
+gameResultType coinToss(unsigned int initial, unsigned int target) {
+  // initialize variables
+  gameResultType game;
 
-    //while game end condition aren't met
-    while (gameResultType.amount < target && gameResultType.amount > 0){
-        game.numToss++; //add a coin toss
-        betAmount = ((game.amount%2) ? (game.amount)/2 + 1 : game.amount/2)
-        coin = rand()%2;
-        if (coin) //if true
-            game.amount += betAmount;
-        else
-            game.amount -= betAmount;
-    }
-    return game
+  game.numToss = 0;
+  game.amount  = initial;
+  int  betAmount = 0;
+  bool coin;
+
+  // while game end condition aren't met
+  while (gameResultType.amount < target && gameResultType.amount > 0) {
+    game.numToss++; // add a coin toss
+    betAmount =
+      ((game.amount % 2) ? (game.amount) / 2 + 1 : game.amount / 2)
+      coin = rand() % 2;
+
+    if (coin) // if true
+      game.amount += betAmount;
+    else game.amount -= betAmount;
+  }
+  return game;
 }
